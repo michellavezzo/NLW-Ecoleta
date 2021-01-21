bb33b1ca16a565e989fcb1d0d48318f863a567e4
Michel Lavezzo <michellavezzo@users.noreply.github.com>
2021-01-21 19:50:25 -0300

    create-point is now saving data to database, search page is now getting data from database. finished until now

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,21 +1,21 @@
-function populateUFs(){
+function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    //.then((res) => {return res.json()}) //return retorna uma promeça para o then
-    .then( res => res.json())  // igual a linha de cima
-   .then( states => {
+        //.then((res) => {return res.json()}) //return retorna uma promeça para o then
+        .then(res => res.json()) // igual a linha de cima
+        .then(states => {
 
-        for( const state of states ){
-            ufSelect.innerHTML += `<option value = "${state.id}">${state.nome}</option>`
-        }
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value = "${state.id}">${state.nome}</option>`
+            }
 
-        
-    })
+
+        })
 }
 populateUFs()
 
-function getCities(event){
+function getCities(event) {
     const citySelect = document.querySelector("[name=city]")
     const stateInput = document.querySelector("[name=state]")
     console.log(event.target.value)
@@ -32,16 +32,16 @@ function getCities(event){
     citySelect.disabled = true;
 
     fetch(url)
-    .then( res => res.json() )  // igual a linha de cima
-    .then( cities => {
+        .then(res => res.json()) // igual a linha de cima
+        .then(cities => {
 
 
-        for( const city of cities ){
-            citySelect.innerHTML += `<option value = "${city.nome}">${city.nome}</option>`
-        }
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value = "${city.nome}">${city.nome}</option>`
+            }
 
-        citySelect.disabled = false;
-    })
+            citySelect.disabled = false;
+        })
 
 }
 
@@ -53,7 +53,7 @@ document
 // pegar todos os li's
 const itemsToCollect = document.querySelectorAll(".items-grid li")
 
-for(const item of itemsToCollect ){
+for (const item of itemsToCollect) {
     item.addEventListener("click", handleSelectedItem)
 }
 
@@ -61,38 +61,41 @@ for(const item of itemsToCollect ){
 const collectedItems = document.querySelector("input[name = items]")
 let selectedItems = [] //array de items
 
-function handleSelectedItem(event){
+function handleSelectedItem(event) {
     // add or remove class on JS
     const itemLi = event.target
 
     itemLi.classList.toggle("selected") //add or remove class
-    
+
     const itemId = itemLi.dataset.id
 
+
+    // console.log('ITEM-ID: ', itemId)
     //verificar se existem itens selecionados, se sim
     //se sim, pegar os itens selecionados
-    const alreadySelected = selectedItems.findIndex( item => item == itemId)
-    // parte de cima tem a msm funçao
-    // const alreadySelected = selectedItems.findIndex( item => {
-    //     const itemFound = item == itemId
-    //     return itemFound
-    // })
+    const alreadySelected = selectedItems.findIndex(item => item == itemId)
+        // parte de cima tem a msm funçao
+        // const alreadySelected = selectedItems.findIndex( item => {
+        //     const itemFound = item == itemId
+        //     return itemFound
+        // })
 
 
     //se ja estiver selecionado, tirar da selecao
-    if(alreadySelected >= 0){
+    if (alreadySelected >= 0) {
         //tirar da seleçao
-        const filteredItems = selectedItems.filter( item => {
+        const filteredItems = selectedItems.filter(item => {
             const itemIsDifferent = item != itemId
-            return itemIsDifferent 
+            return itemIsDifferent
         })
 
         selectedItems = filteredItems
-    }else{
+    } else {
         // se nao estiveer selecionado, adiocionar a selecao
         selectedItems.push(itemId)
     }
 
+    // console.log('selectedItems: ', selectedItems)
     //atualizae o campo escondido com os itens selecionados    
     collectedItems.value = selectedItems
 
--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db")
 //config. public folder
 server.use(express.static("public"))
 
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
+
 //utilizando template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -21,13 +24,69 @@ server.get("/", (req, res) => {
     return res.render("index.html", )
 })
 server.get("/create-point", (req, res) => {
+
+    // req.query: Query strings da url 
+    console.log(req.query)
+
     return res.render("create-point.html")
 })
+server.post("/savepoint", (req, res) => {
+
+    // console.log(req.body)
+
+    //inserir dados no banco de dados
+
+    const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city, 
+                items
+            ) VALUES (?,?,?,?,?,?,?);
+        `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+                // criar pagina de erro
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+
+})
+
 server.get("/search", (req, res) => {
+
+    const search = req.query.search
+    if (search == "") {
+        //pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
 
     //pegar dados do banco de dados
     //consultar dados da tabela
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if (err) {
             return console.log(err)
         }
